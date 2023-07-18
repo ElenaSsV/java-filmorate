77760a84c26f52f3b9439d070a994e7658ba85e9
Elena Semseeva <e.semseeva@yandex.ru>
2023-07-18 16:05:07 +0300

    style: empty lines added

--- src/main/java/ru/yandex/practicum/filmorate/service/CommonService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/CommonService.java
@@ -5,8 +5,12 @@ import ru.yandex.practicum.filmorate.model.Entity;
 import java.util.List;
 
 public interface CommonService<T extends Entity> {
-    T create (T data);
+
+    T create(T data);
+
     T update(T data);
+
     List<T> getAll();
+
     T getById(long id);
 }
